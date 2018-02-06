a5997551e140a7385dbb01917dc905df3e143c48
Praneeth Gurram <praneeth.gurram@itpeoplecorp.com>
2018-02-06 08:32:17 -0500

    testbook

---
+++ testbook/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: testbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'tree' do
+
+end
+
---
+++ testbook/recipes/learn.rb
@@ -0,0 +1,34 @@
+package 'apache' do
+	package_name 'apache2'
+end
+
+service 'apache2' do
+	action [:enable, :start]
+end
+
+file '/var/www/index.html' do
+	action :delete
+end
+
+file '/var/www/html/index.html' do
+	content 'Hello world'
+	owner 'praneeth'
+end
+
+file '/home/praneeth/Builds/Chefplaybook/test.html' do
+	content 'created in chefplaybook dir'
+	
+end
+
+execute 'command-test' do
+
+	command 'echo blah >> /home/praneeth/Builds/Chefplaybook/test.html'
+	only_if 'test -r /home/praneeth/Builds/Chefplaybook/test.html'
+
+end
+
+ 
+
+	
+
+
